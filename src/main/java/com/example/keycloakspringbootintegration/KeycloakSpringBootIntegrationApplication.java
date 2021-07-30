b36ac0fc10dce2383a1c703df0468be5848f236e
unknown <sachinmewar800@gmail.com>
2021-07-30 12:43:16 +0530

    src dir

---
+++ src/main/java/com/example/keycloakspringbootintegration/KeycloakSpringBootIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloakspringbootintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringBootIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakSpringBootIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keycloakspringbootintegration/KeycloakSpringBootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloakspringbootintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringBootIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
